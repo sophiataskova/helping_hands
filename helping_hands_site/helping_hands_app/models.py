1e217371390f3d15c1eff5902c986fb9b46daf5e
Sofiya Taskova <sophiataskova@yahoo.co.uk>
2015-10-19 19:22:29 -0700

    added events to admin page

--- helping_hands_site/helping_hands_app/models.py
+++ helping_hands_site/helping_hands_app/models.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 
 
@@ -6,10 +8,13 @@ class Event(models.Model):
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):
         return self.event_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     event = models.ForeignKey(Event)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __unicode__(self):
-	return self.votes
+	return self.choice_text
