cf84bd0ecd6c00fe4064990f559dd7131e71e855
Sofiya Taskova <sophiataskova@yahoo.co.uk>
2015-10-19 18:16:58 -0700

    some models

--- helping_hands_site/helping_hands_app/models.py
+++ helping_hands_site/helping_hands_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    event_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.event_text
+
+class Choice(models.Model):
+    event = models.ForeignKey(Event)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __unicode__(self):
+	return self.votes
