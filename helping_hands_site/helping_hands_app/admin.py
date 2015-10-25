39fd26b2e1f4c27cb8cfc378eb70d8203ed9fac2
Sofiya Taskova <sophiataskova@yahoo.co.uk>
2015-10-25 16:45:42 -0700

    some admin page customization

--- helping_hands_site/helping_hands_app/admin.py
+++ helping_hands_site/helping_hands_app/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from helping_hands_app.models import Event
+from helping_hands_app.models import Choice
 
-admin.site.register(Event)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['event_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('event_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['event_text']
+    date_hierarchy = 'pub_date'
+
+admin.site.register(Event, EventAdmin)
---
+++ helping_hands_site/helping_hands_site/helping_hands_site/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'helping_hands_site.views.home', name='home'),
+    # url(r'^helping_hands_site/', include('helping_hands_site.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'helping_hands_app.views.index')
+)
