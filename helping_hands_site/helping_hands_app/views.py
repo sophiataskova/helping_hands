3329d7edf17a1b9255e65d2770a3020e54a40932
Sofiya Taskova <sophiataskova@yahoo.co.uk>
2015-10-25 19:28:32 -0700

    somewhat working version

--- helping_hands_site/helping_hands_app/views.py
+++ helping_hands_site/helping_hands_app/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render_to_response, render, get_object_or_404
+from django.template import RequestContext, loader
+from django.http import HttpResponse
+from django.http import Http404
+
+from helping_hands_app.models import Event
 
 def index(request):
-    return render_to_response('index.html')
+    latest_event_list = Event.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_event_list': latest_event_list}
+    return render(request, 'helping_hands_app/index.html', context)
+
+def detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    return render(request, 'helping_hands_app/detail.html', {'event': event})
+
+def results(request, event_id):
+    return HttpResponse("You're looking at the votes of event %s." % event_id)
+
+def vote(request, event_id):
+    return HttpResponse("You're voting on event %s." % event_id)
--- helping_hands_site/helping_hands_site/urls.py
+++ helping_hands_site/helping_hands_site/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
+from helping_hands_app import views
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -14,5 +14,10 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'helping_hands_app.views.index')
+    url(r'^$', 'helping_hands_app.views.index', name='index'),
+    url(r'^events/', include('helping_hands_site.urls')),
+    url(r'^(?P<event_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<event_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<event_id>\d+)/vote/$', views.vote, name='vote'),
 )
+
